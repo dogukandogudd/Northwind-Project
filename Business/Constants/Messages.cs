d938c82a7f1ea13cbc6a5f3579a3ac7423ddfe0a
Doğukan <dogukandogu8@gmail.com>
2021-02-11 00:18:27 +0300

    10 Şubat

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "+Product Added.+";
+        public static string ProductNameInvalid = "*product name is invalid.*";
+        public static string MaintenanceTime = "system is under maintenance";
+        public static string ProductsListed = "products listed";
+    }
+}
